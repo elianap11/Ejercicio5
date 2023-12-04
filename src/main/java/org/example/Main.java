d5ba02dfe9506a32d98d3d5c404b1952897c62be
Eliana Paniagua <elianaprensa@gmail.com>
2023-12-04 16:02:02 -0300

    agrego restricción solo letras inglés

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         String[] myArray = {"FirstWord", "SecondWord", "THIRDWORD"};
 
-        String result = "";
-
-        for (String word : myArray) {
-
-            result += word.toLowerCase() + " ";
-
+        if (myArray.length <= 10) {
+            StringBuilder result = new StringBuilder();
+            for (String word : myArray) {
+                String filteredWord = word.replaceAll("[^a-zA-Z]", "");
+                result.append(filteredWord.toLowerCase()).append(" ");
+            }
+            System.out.println(result.toString().trim());
+        } else {
+            System.out.println("La longitud del array supera el límite permitido");
         }
-
-        result = result.trim();
-
-        System.out.println(result);
-
     }
-
 }
